c75d097e7e6aae2470e40b11eb1328e13402cd77
Joakim Lavenius <theguidingblue@gmail.com>
2012-11-16 10:58:53 +0100

    Test

--- Joakim och Kims projekt/src/kimpaket/Startklass.java
+++ Joakim och Kims projekt/src/kimpaket/Startklass.java
@@ -9,19 +9,27 @@ public class Startklass {
 		// TODO Auto-generated method stub
 
 		
-		//Variabler för antalet djur
+		//Variabler för antalet djur. Om ett djur dör eller blir lik, ändra på dessa.
+		//Gör en "djurnamn = null" och numberOfAnimal++ eller numberOfAnimal-- vid behov.
 		int numberOfAntiloop = 0;
 		int numberOfCarcass = 0;
 		int numberOfHyena = 0;
 		int numberOfLion = 0;
 		
+		
+		//Exempel
 		Lion simba = new Lion();
 		numberOfLion++;
-		simba.eat();
+		simba.eat(numberOfAntiloop, numberOfHyena);
 		System.out.println(simba.hunger);
 		
 		simba = null;
+		numberOfLion--;
 		
+		
+		//Antal djur och lik i parken
+		System.out.println("Det finns " + numberOfLion + " lejon, " + numberOfHyena 
+		+ " hyenor, " + numberOfAntiloop + " antiloper och " + numberOfCarcass + " ouppätna lik i savannen.");
 	}
 
 }
