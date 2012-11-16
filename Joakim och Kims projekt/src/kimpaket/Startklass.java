d6231e741dcc93260a8047b2a845045722580e6b
Joakim Lavenius <theguidingblue@gmail.com>
2012-11-16 10:08:14 +0100

    nya variabler i startklass

--- Joakim och Kims projekt/src/kimpaket/Startklass.java
+++ Joakim och Kims projekt/src/kimpaket/Startklass.java
@@ -8,6 +8,10 @@ public class Startklass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int numberOfAntiloop = 0;
+		int numberOfHyena = 0;
+		int numberOfLion = 0;
+		
 		Lion simba = new Lion();
 		simba.eat();
 		System.out.println(simba.hunger);
